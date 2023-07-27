24560630618a73df7daf8d2625d8d42fbcb16b3a
oguzhankodaz <oguzhankodaz@gmail.com>
2023-07-27 16:42:47 +0300

    category-item.component olis created and codes updades

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.component';
 const App = () => {
 
   const categories = [
@@ -31,22 +32,10 @@ const App = () => {
   
   return (
     <div className="categories-container">
-      {categories.map(({ title,id,imageUrl }) =>(
+      {categories.map((category) =>(
+        <CategoryItem key={category.id} category={category} ></CategoryItem>
     
-        <div key={id} className="category-container">
-
-          <div  className="background-image" 
-          style={{
-            backgroundImage:`url(${imageUrl})`
-            
-          }}
-           
-          > </div>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now 1</p>
-          </div>
-        </div>)
+  )
 
       )}
 
---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,24 @@
+import  './category-item.styles.scss'
+
+const CategoryItem = ({category}) =>{
+    const {imageUrl,title } = category;
+
+    return (
+        <div  className="category-container">
+        <div  className="background-image" 
+        style={{
+          backgroundImage:`url(${imageUrl})`
+        }}
+         
+        > </div>
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Shop Now 1</p>
+        </div>
+      </div>
+    )
+
+    
+}
+
+export default CategoryItem;
